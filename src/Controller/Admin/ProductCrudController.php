c0929dde0efbf3f389f102fa2f3565bc85bc357e
Marcus Reis <mvsantosreis@gmail.com>
2024-07-01 20:23:19 +0100

    Persist Product Dashboard

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,26 +3,58 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProductCrudController extends AbstractCrudController
 {
+    public const PRODUCTS_BASE_PATH = 'upload/images/products';
+    public const PRODUCTS_UPLOAD_DIR = 'public/upload/images/products';
+
     public static function getEntityFqcn(): string
     {
         return Product::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name', 'Label'),
             TextEditorField::new('description'),
+            MoneyField::new('price')->setCurrency('EUR'),
+
+            ImageField::new('image')
+                ->setBasePath(self::PRODUCTS_BASE_PATH)
+                ->setUploadDir(self::PRODUCTS_UPLOAD_DIR)
+                ->setSortable(false),
+
+            BooleanField::new('active'),
+
+            AssociationField::new('category')->setQueryBuilder(function (QueryBuilder $queryBuilder) {
+                $queryBuilder->where('entity.active = true');
+            }),
+
+            DateTimeField::new('updatedAt')->hideOnForm(),
+            DateTimeField::new('createdAt')->hideOnForm(),
         ];
     }
-    */
+
+    public function persistEntity(EntityManagerInterface $em, $entityInstance): void
+    {
+        if (!$entityInstance instanceof Product) return;
+
+        $entityInstance->setCreatedAt(new \DateTimeImmutable);
+
+        parent::persistEntity($em, $entityInstance);
+    }
 }
